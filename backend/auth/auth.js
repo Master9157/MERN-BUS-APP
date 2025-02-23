83f3e03f5cd60dca049b0d2ba27c91cc7b1a81ca
master9157 <owen.grant6139@hotmail.com>
2025-02-23 01:27:48 -0500

    feat #1: add body-parser module

--- backend/auth/auth.js
+++ backend/auth/auth.js
@@ -1,3 +1,5 @@
 const passport = require("passport");
 const localStrategy = require("passport-local").Strategy;
 const User = require("../models/User");
+var bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
